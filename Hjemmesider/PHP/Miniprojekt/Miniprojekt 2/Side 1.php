5f93b61112351895833c365cac8c1e6107bf796c
Mathias <Math86m5@edu.sde.dk>
2020-10-12 20:01:27 +0200

    Hjemmefra

--- Hjemmesider/PHP/Miniprojekt/Miniprojekt 2/Side 1.php
+++ Hjemmesider/PHP/Miniprojekt/Miniprojekt 2/Side 1.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Miniprojekt Side 1</title>
+    <title>Miniprojekt side 1</title>
 </head>
 <body>
 <?php
