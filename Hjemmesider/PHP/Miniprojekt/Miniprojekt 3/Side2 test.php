ad8bbb847b9fccc6978b0062c3eef7ee3ac99f0f
Mathias <Math86m5@edu.sde.dk>
2020-11-14 13:45:26 +0100

    hjemmefra

--- Hjemmesider/PHP/Miniprojekt/Miniprojekt 3/Side2 test.php
+++ Hjemmesider/PHP/Miniprojekt/Miniprojekt 3/Side2 test.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Miniprojekt 3</title>
+    <title>Miniprojekt side 3</title>
 </head>
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST">
